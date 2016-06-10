c859d16cba9035947e69a9b1e7d7293d85396ee9
Daniel Mills <danielmillst@gmail.com>
2016-06-10 18:10:27 -0400

    Update to 2.2.5

--- src/org/cyberpwn/react/Version.java
+++ src/org/cyberpwn/react/Version.java
@@ -2,8 +2,8 @@ package org.cyberpwn.react;
 
 public class Version
 {
-	public static final int C = 2204;
-	public static final String V = "2.2.4p";
+	public static final int C = 2205;
+	public static final String V = "2.2.5p";
 	public static final String E = ".rcf.dex";
 	public static final String K = "F";
 }
