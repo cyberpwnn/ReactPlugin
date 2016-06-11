b4a2d7cf973880a641120b0030b247d7afb56a20
Daniel Mills <danielmillst@gmail.com>
2016-06-11 08:24:58 -0400

    v2.2.6

--- src/org/cyberpwn/react/Version.java
+++ src/org/cyberpwn/react/Version.java
@@ -2,8 +2,8 @@ package org.cyberpwn.react;
 
 public class Version
 {
-	public static final int C = 2205;
-	public static final String V = "2.2.5p";
+	public static final int C = 2206;
+	public static final String V = "2.2.6p";
 	public static final String E = ".rcf.dex";
 	public static final String K = "F";
 }
