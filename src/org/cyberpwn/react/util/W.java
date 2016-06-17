fdcc6e6b721c18a2c60349668e0b314556b8978b
Daniel Mills <danielmillst@gmail.com>
2016-06-17 01:17:38 -0400

    World Utils

---
+++ src/org/cyberpwn/react/util/W.java
@@ -0,0 +1,26 @@
+package org.cyberpwn.react.util;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class W
+{
+	public static Location fall(Location from)
+	{
+		int height = from.getBlockY();
+		
+		for(int i = height; i > 0; i++)
+		{
+			int check = i - 1;
+			
+			Material type = new Location(from.getWorld(), from.getBlockX(), check, from.getBlockZ()).getBlock().getType();
+			
+			if(!(type.equals(Material.AIR) || type.equals(Material.WATER) || type.equals(Material.STATIONARY_WATER) || type.equals(Material.LAVA) || type.equals(Material.STATIONARY_LAVA)))
+			{
+				return new Location(from.getWorld(), from.getBlockX(), check - 1, from.getBlockZ());
+			}
+		}
+		
+		return null;
+	}
+}
