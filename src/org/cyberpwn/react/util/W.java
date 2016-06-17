013d6d4ad14c2793edfc4cc1900d61ee5fa96d22
Daniel Mills <danielmillst@gmail.com>
2016-06-17 02:21:37 -0400

    Make it actually work

--- src/org/cyberpwn/react/util/W.java
+++ src/org/cyberpwn/react/util/W.java
@@ -9,7 +9,7 @@ public class W
 	{
 		int height = from.getBlockY();
 		
-		for(int i = height; i > 0; i++)
+		for(int i = height; i > 0; i--)
 		{
 			int check = i - 1;
 			
@@ -17,7 +17,7 @@ public class W
 			
 			if(!(type.equals(Material.AIR) || type.equals(Material.WATER) || type.equals(Material.STATIONARY_WATER) || type.equals(Material.LAVA) || type.equals(Material.STATIONARY_LAVA)))
 			{
-				return new Location(from.getWorld(), from.getBlockX(), check - 1, from.getBlockZ());
+				return new Location(from.getWorld(), from.getBlockX(), check + 1, from.getBlockZ());
 			}
 		}
 		
