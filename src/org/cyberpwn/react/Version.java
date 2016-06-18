a743ac40d86ea4b9a4a42ee2d5611bd17cd3489c
Daniel Mills <danielmillst@gmail.com>
2016-06-18 09:49:15 -0400

    2.4.1 stuff

--- src/org/cyberpwn/react/Version.java
+++ src/org/cyberpwn/react/Version.java
@@ -2,6 +2,6 @@ package org.cyberpwn.react;
 
 public class Version
 {
-	public static final int C = 2400;
-	public static final String V = "2.4p";
+	public static final int C = 2401;
+	public static final String V = "2.4.1p";
 }
