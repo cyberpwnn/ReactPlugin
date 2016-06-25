ebe9a190e3139869fe148f2145443095e23045c5
Daniel Mills <danielmillst@gmail.com>
2016-06-25 19:03:01 -0400

    Task executer

---
+++ src/org/cyberpwn/react/controller/TaskController.java
@@ -0,0 +1,36 @@
+package org.cyberpwn.react.controller;
+
+import org.cyberpwn.react.React;
+import org.cyberpwn.react.util.GList;
+import org.cyberpwn.react.util.M;
+
+public class TaskController extends Controller
+{
+	private GList<Runnable> tasks;
+	
+	public TaskController(React react)
+	{
+		super(react);
+		
+		tasks = new GList<Runnable>();
+	}
+	
+	public void tick()
+	{
+		long ms = M.ms();
+		
+		while(!tasks.isEmpty() && M.ms() - ms < 3)
+		{
+			tasks.get(0).run();
+		}
+	}
+	
+	/**
+	 * Run a task, even if you are asyncronous, it will be executed sync
+	 * @param r runnable
+	 */
+	public void newTask(Runnable r)
+	{
+		tasks.add(r);
+	}
+}
