9785f06f22220eb6b572c308058c6b1a34c6d40d
Daniel Mills <danielmillst@gmail.com>
2016-06-26 05:26:44 -0400

    Pre VChange

--- src/org/cyberpwn/react/Version.java
+++ src/org/cyberpwn/react/Version.java
@@ -2,6 +2,6 @@ package org.cyberpwn.react;
 
 public class Version
 {
-	public static final int C = 2601;
-	public static final String V = "2.6.1p";
+	public static final int C = 2603;
+	public static final String V = "2.6.3p";
 }
