877760fcd2305d4c6a33634ff94360ea13bd915a
Daniel Mills <danielmillst@gmail.com>
2016-07-01 20:09:45 -0400

    Dem Photons

--- src/org/cyberpwn/react/controller/PhotonController.java
+++ src/org/cyberpwn/react/controller/PhotonController.java
@@ -7,6 +7,10 @@ import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 import org.cyberpwn.react.React;
 import org.cyberpwn.react.cluster.ClusterConfig;
 import org.cyberpwn.react.cluster.Configurable;
@@ -17,39 +21,65 @@ import org.cyberpwn.react.util.ExecutiveRunnable;
 import org.cyberpwn.react.util.F;
 import org.cyberpwn.react.util.GList;
 import org.cyberpwn.react.util.Timer;
+import org.cyberpwn.react.util.W;
 
-public class LightController extends Controller implements Configurable
+public class PhotonController extends Controller implements Configurable
 {
-	private Boolean running;
 	private ClusterConfig cc;
+	private GList<Chunk> photons;
+	private GList<Chunk> prec;
 	private GList<Chunk> cache;
+	private Integer k;
 	
-	public LightController(React react)
+	public PhotonController(React react)
 	{
 		super(react);
 		
 		this.cc = new ClusterConfig();
 		this.cache = new GList<Chunk>();
-		this.running = false;
+		this.prec = new GList<Chunk>();
+		this.photons = new GList<Chunk>();
+		this.k = 0;
 	}
 	
 	@Override
 	public void tick()
 	{
-		if(!running && !cache.isEmpty())
+		if(k > 100 && photons.size() < cc.getInt("photon.relight.limits.max-pool-size") && !cache.isEmpty() && !getReact().getActionController().getActionInstabilityCause().isLagging())
 		{
-			running = true;
+			photons.add(cache.get(0));
 			relight(cache.get(0));
+			prec.add(cache.get(0));
+			cache.pop();
+			
+			if(photons.size() < cc.getInt("photon.relight.limits.max-pool-size"))
+			{
+				k = 100;
+			}
+			
+			else
+			{
+				k = 0;
+			}
 		}
+		
+		k++;
+	}
+	
+	public long getLimit()
+	{
+		return 1 + (cc.getInt("photon.relight.limits.max-ms") / photons.size());
 	}
 	
 	@Override
 	public void onNewConfig()
 	{
-		cc.set("photon.relight.enabled", false);
-		cc.set("photon.relight.limits.max-ms", 2);
+		cc.set("photon.relight.enabled", true);
+		cc.set("photon.relight.limits.max-ms", 8);
+		cc.set("photon.relight.limits.max-pool-size", 4);
 		cc.set("photon.relight.limits.max-cache-size", 1024);
 		cc.set("photon.relight.limits.halt-on-lag", true);
+		cc.set("photon.relight.constraints.chunk-radius", 3);
 	}
 	
 	@Override
@@ -82,15 +112,42 @@ public class LightController extends Controller implements Configurable
 			return;
 		}
 		
-		if(!cache.contains(c))
+		if(!cache.contains(c) && !prec.contains(c))
 		{
-			cache.add(c);
+			cache.addFirst(c);
 		}
 	}
 	
 	public boolean visible(Block block)
 	{
 		return block.getRelative(BlockFace.UP).getType().equals(Material.AIR) || block.getRelative(BlockFace.NORTH).getType().equals(Material.AIR) || block.getRelative(BlockFace.SOUTH).getType().equals(Material.AIR) || block.getRelative(BlockFace.EAST).getType().equals(Material.AIR) || block.getRelative(BlockFace.WEST).getType().equals(Material.AIR) || block.getRelative(BlockFace.DOWN).getType().equals(Material.AIR);
+	}
+	
+	@EventHandler
+	public void onChunk(ChunkUnloadEvent e)
+	{
+		prec.remove(e.getChunk());
+		cache.remove(e.getChunk());
+	}
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e)
+	{
+		if(!e.getFrom().getChunk().equals(e.getTo().getChunk()))
+		{
+			if(cc.getInt("photon.relight.constraints.chunk-radius") > 0)
+			{
+				for(Chunk i : W.crad(e.getTo().getChunk(), cc.getInt("photon.relight.constraints.chunk-radius")))
+				{
+					request(i);
+				}
+			}
+			
+			else
+			{
+				request(e.getTo().getChunk());
+			}
+		}
 	}
 	
 	public void relight(final Chunk chunk)
@@ -100,16 +157,26 @@ public class LightController extends Controller implements Configurable
 		final GList<Block> intended = new GList<Block>();
 		final Iterator<Block> bks = new Cuboid(chunk.getBlock(0, 0, 0).getLocation(), chunk.getBlock(15, 255, 15).getLocation()).iterator();
 		final Integer[] cy = new Integer[] { 0 };
-				
+		
 		t.start();
 		
-		new ExecutiveIterator<Block>(cc.getInt("photon.relight.limits.max-ms").longValue(), bks, new ExecutiveRunnable<Block>()
+		final String s = "lim: " + getLimit();
+		new ExecutiveIterator<Block>(getLimit(), bks, new ExecutiveRunnable<Block>()
 		{
+			@SuppressWarnings("deprecation")
 			public void run()
 			{
 				if(!next().getType().equals(Material.AIR) && visible(next()))
 				{
 					intended.add(next());
+					
+					Block b = next();
+					Material m = b.getType();
+					BlockState bs = b.getState();
+					b.setType(Material.STONE);
+					b.setType(m);
+					b.setData(bs.getRawData());
+					b.getState().update(false, true);
 				}
 			}
 		}, new Runnable()
@@ -128,9 +195,8 @@ public class LightController extends Controller implements Configurable
 					public void run()
 					{
 						t.stop();
-						s("Relit in " + F.nsMs(t.getTime(), 0) + "ms " + ChatColor.AQUA + cy[0] + " blocks lit over " + F.f(((double) t.getTime() / 1000000.0) / 50.0) + " ticks" + ChatColor.LIGHT_PURPLE + " visible: " + F.pc(((double)cy[0] / 65536.0), 3) + ChatColor.RED + " " + cache.size() + " left");
-						cache.remove(chunk);
-						running = false;
+						s("Relit in " + F.nsMs(t.getTime(), 0) + "ms " + ChatColor.AQUA + cy[0] + " blocks lit over " + F.f(((double) t.getTime() / 1000000.0) / 50.0) + " ticks" + ChatColor.LIGHT_PURPLE + " visible: " + F.pc(((double) cy[0] / 65536.0), 3) + ChatColor.RED + " " + photons.size() + " in pool " + ChatColor.YELLOW + cache.size() + " cached" + ChatColor.BLUE + " " + s + " " + ChatColor.GOLD + "prec: " + prec.size());
+						photons.remove(chunk);
 					}
 				});
 			}
