1a14f54f769f589385aa0f71919850da78a62894
Daniel Mills <danielmillst@gmail.com>
2016-07-05 09:07:28 -0400

    Shitty tests

---
+++ src/test/java/org/cyberpwn/react/tests/X.java
@@ -0,0 +1,9 @@
+package org.cyberpwn.react.tests;
+
+public class X
+{
+	public static void x(String cat, String name)
+	{
+		System.out.println("TESTING > " + cat.toUpperCase().replace("TEST", "") + " > " + name.toUpperCase());
+	}
+}
