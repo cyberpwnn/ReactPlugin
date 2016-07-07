b57b01e5591eab20e620f6cca256bc8519b4d352
Daniel Mills <danielmillst@gmail.com>
2016-07-07 17:40:37 -0400

    Suggestions

---
+++ src/main/java/org/cyberpwn/react/controller/SuggestionController.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.react.controller;
+
+import org.cyberpwn.react.React;
+
+public class SuggestionController extends Controller
+{
+	public SuggestionController(React react)
+	{
+		super(react);
+	}
+}
